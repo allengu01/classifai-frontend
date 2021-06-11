903a5ec134087dfcc3291ceed832c857a2fe8725
Eric Wang <ewang1@mit.edu>
2021-06-11 04:33:00 -0500

    reorganized

---
+++ classifai-frontend/Views/Results.swift
@@ -0,0 +1,22 @@
+//
+//  Results.swift
+//  classifai-frontend
+//
+//  Created by Eric Wang on 6/11/21.
+//
+
+// This will be the results page, "ILL DO IT" in figma
+
+import SwiftUI
+
+struct Results: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Results_Previews: PreviewProvider {
+    static var previews: some View {
+        Results()
+    }
+}
