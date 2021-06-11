2fd8f23a350ffdca6b735840b4c9f5464722cdbd
allengu01 <allen.gu01@gmail.com>
2021-06-11 02:49:19 -0500

    Added fonts and logo

---
+++ frontend/Views/Logo.swift
@@ -0,0 +1,28 @@
+//
+//  Logo.swift
+//  frontend
+//
+//  Created by Allen Gu on 6/10/21.
+//
+
+import SwiftUI
+
+struct Logo: View {
+    var body: some View {
+        HStack(alignment: .center, spacing: 0.0) {
+            Text("classif")
+                .font(Fonts.getFont(.nunito, .bold, 36))
+                .foregroundColor(Color(red: 109/255, green: 158/255, blue: 243/255))
+                
+            Text("ai")
+                .font(Fonts.getFont(.nunito, .bold, 36))
+                .foregroundColor(Color(red: 255/255, green: 161/255, blue: 141/255))
+        }
+    }
+}
+
+struct Logo_Previews: PreviewProvider {
+    static var previews: some View {
+        Logo()
+    }
+}
