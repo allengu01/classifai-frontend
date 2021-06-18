436faf8dbce45bdb22ee939f82e7ef1e2d00537a
Eric Wang <ewang1@mit.edu>
2021-06-17 20:17:39 -0400

    poop

---
+++ classifai-frontend/Views/Loading/Loading.swift
@@ -0,0 +1,20 @@
+//
+//  Loading.swift
+//  classifai-frontend
+//
+//  Created by Eric Wang on 6/17/21.
+//
+
+import SwiftUI
+
+struct Loading: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Loading_Previews: PreviewProvider {
+    static var previews: some View {
+        Loading()
+    }
+}
